30e7573008465be6bf208b7b01dabe44758bafc1
terence lim <terencelim92@gmail.com>
2018-07-07 21:35:00 +0800

    Revert "mess around with webpack"
    This reverts commit 10a7529d2c12a0817179d7b35773dd51e39df97c.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: './src/js/index.js',
+    entry: './js/index.js',
     output: {
       path: __dirname + '/dist',
       filename: 'bundle.js'
@@ -8,33 +8,5 @@ module.exports = {
         // require("jquery") is external and available
         //  on the global var jQuery
         "jquery": "jQuery"
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.css$/,
-                use: [ "style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [{
-                    loader: "url-loader",
-                    options: {
-                        limit: 8000,
-                        // name: 'img/[hash]-[name].[ext]',
-                    }
-                }]
-            },
-            {
-                test: /\.(eot|ttf|svg|woff|woff2)$/,
-                use: [{
-                    loader: "url-loader",
-                    options: {
-                        limit: 8000,
-                        name: 'fonts/[hash]-[name].[ext]',
-                    }
-                }]
-            },
-        ]
-    }
+      }
   }
