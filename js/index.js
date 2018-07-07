10a7529d2c12a0817179d7b35773dd51e39df97c
terence lim <terencelim92@gmail.com>
2018-07-07 21:25:21 +0800

    mess around with webpack

--- js/index.js
+++
@@ -1,8 +0,0 @@
-require("./jquery.js");
-require("./ajaxchimp.js");
-require("./main.js");
-require("./nicescroll.js");
-require("./owl.carousel.min.js");
-require("./parallax.js");
-require("./scrollTo.js");
-require("./wow.js");
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+require("./jquery.js");
+require("./ajaxchimp.js");
+require("./main.js");
+require("./nicescroll.js");
+require("./owl.carousel.min.js");
+require("./parallax.js");
+require("./scrollTo.js");
+require("./wow.js");
